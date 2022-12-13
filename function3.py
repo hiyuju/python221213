f1f76a2502e516880caf8683b37be6d3cde4a735
hiyuju <yuju0821@outlook.kr>
2022-12-13 15:49:18 +0900

    함수연습 7

--- function3.py
+++ function3.py
@@ -35,3 +35,55 @@ def intersect(prelist,postlist):
 
 #호출
 print(intersect("HAM","SPAM"))
+
+#함수의 기본인자값
+def times(a=10,b=20):
+    return a*b
+
+#호출 
+print(times())
+print(times(5))
+print(times(5,6))
+
+#키워드인자 파라메터명 명시
+def connectURI(server,port):
+    strURL ="http:\\"+server+":"+port
+    return strURL
+
+#호출
+print( connectURI("credu.com","80"))
+print( connectURI(port="80",server="credu.com"))
+
+#가변적인 경우(가변인자*)
+def union(*ar):
+    result=[]
+    for item in ar:
+        for x in item:
+            if x not in result:
+                result.append(x)
+    return result
+
+#호출
+print ( union("HAM","EGG"))
+print ( union("HAM","EGG", "SPAM"))
+
+#정의되지 않은 인자(필수와 옵션이 같이 있는 경우 **)
+def userURIbuilder(server,port,**user):
+    strURL="http:\\"+server+":"+port+"/?"
+    for key in user.keys():
+        strURL+=key+"="+user[key]+"&"
+    return strURL
+
+#호출
+print(userURIbuilder("credu.com","80", id="kim", passwd="1234"))
+print(userURIbuilder("credu.com","80", id="kim", passwd="1234",
+    age="30"))
+
+#간단하게 함수를 정의하는 문법:람다함수
+#입력 받아서 : 뒤에서 처리
+g=lambda x,y:x*y
+print(g(3,4))
+print(g(5,6))
+#일회성으로 사용하고 버리기
+print((lambda x:x*x)(3))
+print(globals())
