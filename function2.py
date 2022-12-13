8120070a10edd6e015ae74ccdef20b19fa54773d
hiyuju <yuju0821@outlook.kr>
2022-12-13 13:50:52 +0900

    함수연습3

--- function2.py
+++ function2.py
@@ -1,5 +1,17 @@
 #function2.py
 def setValue(newValue):
+    #지역변수
     x=newValue
+    print(x)
 
+#호출
+#디버깅 중단점
 retValue=setValue(10)
+print(retValue)
+
+#함수정의
+def swap(x,y):
+    return y,x
+
+#호출
+print(swap(3,4))
