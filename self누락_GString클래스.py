c25e674335bb97d4392dddda6d572c91de1bffe9
hiyuju <yuju0821@outlook.kr>
2022-12-14 10:35:57 +0900

    class연습

---
+++ self누락_GString클래스.py
@@ -0,0 +1,17 @@
+#전역변수
+str = "Not Class Member"
+
+#클래스 정의
+class GString:
+    def __init__(self):
+        #멤버 뵨슈
+        self.str = "" 
+    def set(self, msg):
+        self.str = msg
+    def print(self):
+        #명시적 지정
+        print(self.str)
+
+g = GString()
+g.set("First Message")
+g.print()
