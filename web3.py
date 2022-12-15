707447a25c77c05f73dcaa82ca2110687043fa8f
hiyuju <yuju0821@outlook.kr>
2022-12-15 16:11:25 +0900

    웹크롤링 44

--- web3.py
+++ web3.py
@@ -4,24 +4,21 @@ import urllib.request
 #
 from bs4 import BeautifulSoup
 
-#네이버에 요구
-data=urllib.request.urlopen("http://comic.naver.com/webtoon/list.nhn?titleId=20853&weekday=fri")
-#검색이 용이한 객체
-soup=BeautifulSoup(data, "html.parser" )
+#파일에 저장
+f=open("c:\\work\\webtoon.txt", "wt", encoding="utf-8")
+#페이징 처리(각 페이지에 10개의 데이터)
+for i in range(1,6):
+    url="https://comic.naver.com/webtoon/list?titleId=20853&weekday=fri&page="+str(i)
+    print(url)   
+    data=urllib.request.urlopen(url)
+    #검색이 용이한 객체
+    soup=BeautifulSoup(data, "html.parser" )
+    cartoons=soup.find_all("td", class_="title")
+    for tag in cartoons:
+        title=tag.find("a").text
+        print(title.strip())
+        f.write(title.strip()+"\n")
 
-cartoons=soup.find_all("td", class_="title")
-print("길이:{0}".format(len(cartoons)))
-title =cartoons[0].find("a").text
-link=cartoons[0].find("a")["href"]
-print(title.strip())
-print(link.strip())
-
-	# <td class="title">
-	# <a href="/webtoon/detail?titleId=20853&no=50&weekday=fri" onclick="nclk_v2(event,'lst.title','20853','50')">마음의 소리 50화 &lt;격렬한 나의 하루&gt;</a>
-	# </td>
-
-for tag in cartoons:
-    title=tag.text.strip()
-    print(title)
+f.close()
 
     
