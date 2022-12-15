c89a233991216ae39c5a1c4ae09bd6af47cd07d9
hiyuju <yuju0821@outlook.kr>
2022-12-15 14:51:12 +0900

    웹크롤링 연습2

--- web1.py
+++ web1.py
@@ -1,9 +1,17 @@
 #web1.py
 from bs4 import BeautifulSoup
 
-#페이지 로딩
-page =open ("c:\\work\\test01.html", "rt", encoding="utf-8").open()
+#페이지 로딩: 메서드 체인(연속으로 함수, 메서드 호출)
+page =open ("c:\\work\\test01.html", "rt", encoding="utf-8").read()
 #검색이 용이한 객체
 soup=BeautifulSoup(page, "html.parser")
 #전체
 print(soup.prettify())
+
+#<p>태그 전부
+#print(soup.find_all("p"))
+
+#print(soup.find("p"))
+
+
+#print(soup.find_all("p", class_="outer-text"))
